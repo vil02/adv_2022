e49ba6ece50759db12bf7724a3b72545553d7f0a
piotr.idzik <vil02@o2.pl>
2022-12-01 06:21:24 +0100

    feat: add adv_2022_01

---
+++ solutions/adv_2022_01.py
@@ -0,0 +1,35 @@
+"""
+solution of adv_2022_01
+"""
+
+
+def parse_input(in_str):
+    """returns a list of lists of integers"""
+
+    def proc_piece(in_piece_str):
+        return [int(_) for _ in in_piece_str.splitlines()]
+
+    piece_list = in_str.split("\n\n")
+    return [proc_piece(_) for _ in piece_list]
+
+
+def _count_sums(in_data):
+    return [sum(_) for _ in in_data]
+
+
+def solve_a(in_str):
+    """
+    returns the solution for part_a
+    """
+    data = parse_input(in_str)
+    return max(_count_sums(data))
+
+
+def solve_b(in_str):
+    """
+    returns the solution for part_a
+    """
+    data = parse_input(in_str)
+    sorted_sums = sorted(_count_sums(data))
+    assert len(sorted_sums) >= 3
+    return sorted_sums[-1] + sorted_sums[-2] + sorted_sums[-3]
---
+++ tests/test_adv_2022_01.py
@@ -0,0 +1,44 @@
+"""
+tests of adv_2022_01
+"""
+
+import pytest
+import general_utils as gu
+import solutions.adv_2022_01 as sol
+
+_DAY_NUM = 1
+
+
+def _data_small():
+    return gu.read_input(_DAY_NUM, "small")
+
+
+def _data_p():
+    return gu.read_input(_DAY_NUM, "p")
+
+
+def test_parse_input():
+    """tests parse_input agains example data"""
+    expected = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+    actual = sol.parse_input(_data_small())
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "input_str,expected", [(_data_small(), 24000), (_data_p(), 69912)]
+)
+def test_solve_a(input_str, expected):
+    """
+    tests solve_a
+    """
+    assert sol.solve_a(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected", [(_data_small(), 45000), (_data_p(), 208180)]
+)
+def test_solve_b(input_str, expected):
+    """
+    tests solve_b
+    """
+    assert sol.solve_b(input_str) == expected
