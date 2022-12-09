96b2617879dd64ae7d4b25709901dbca59a1d566
piotr.idzik <vil02@o2.pl>
2022-12-09 12:39:36 +0100

    feat: add adv_2022_09

---
+++ solutions/adv_2022_09.py
@@ -0,0 +1,84 @@
+"""solution of adv_2022_09"""
+
+import collections
+
+Move = collections.namedtuple("Move", ["direction", "steps"])
+
+
+def parse_input(in_str):
+    """parses the input into..."""
+
+    def _proc_single_line(in_line):
+        move_dir, steps_str = in_line.split(" ")
+        return Move(move_dir, int(steps_str))
+
+    return [_proc_single_line(_) for _ in in_str.splitlines()]
+
+
+def _shift_pos(in_pos, in_shift):
+    return tuple(sum(_) for _ in zip(in_pos, in_shift))
+
+
+def _get_direction(head_pos, tail_pos):
+    return tuple(h - t for (h, t) in zip(head_pos, tail_pos))
+
+
+def move_tail(head_pos, tail_pos):
+    """returns the new tail position"""
+    shift = _get_direction(head_pos, tail_pos)
+    if shift == (0, 0) or all(abs(_) in {0, 1} for _ in shift):
+        return tail_pos
+    if 0 in shift:
+        assert 2 in shift or -2 in shift
+        return _shift_pos(tail_pos, tuple(_ // 2 for _ in shift))
+    return _shift_pos(tail_pos, tuple(_ // abs(_) for _ in shift))
+
+
+def _get_move_direction(in_move_direction):
+    return {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}[in_move_direction]
+
+
+class RopeSimulator:
+    """represents a moving rope"""
+
+    def __init__(self, rope_length=2):
+        assert rope_length >= 2
+        orgin = (0, 0)
+        self._length = rope_length
+        self._knots = [orgin for _ in range(rope_length)]
+        self._visited_by_tail = {orgin}
+
+    def make_move(self, move):
+        """makes a single move"""
+        move_direction = _get_move_direction(move.direction)
+        for _ in range(move.steps):
+            self._knots[0] = _shift_pos(self._knots[0], move_direction)
+            for _ in range(1, self._length):
+                self._knots[_] = move_tail(self._knots[_ - 1], self._knots[_])
+            self._visited_by_tail.add(self._knots[-1])
+
+    def make_moves(self, moves):
+        """makes all of the moves from the moves"""
+        for _ in moves:
+            self.make_move(_)
+
+    @property
+    def visited_by_tail(self):
+        """returns the set of locations visited by the tail"""
+        return self._visited_by_tail
+
+
+def _run_simulation(in_str, rope_length):
+    rope_simulator = RopeSimulator(rope_length)
+    rope_simulator.make_moves(parse_input(in_str))
+    return len(rope_simulator.visited_by_tail)
+
+
+def solve_a(in_str):
+    """returns the solution for part_a"""
+    return _run_simulation(in_str, 2)
+
+
+def solve_b(in_str):
+    """returns the solution for part_b"""
+    return _run_simulation(in_str, 10)
---
+++ tests/test_adv_2022_09.py
@@ -0,0 +1,151 @@
+"""tests of adv_2022_09"""
+
+import pytest
+import general_utils as gu
+import solutions.adv_2022_09 as sol
+
+_DAY_NUM = 9
+
+
+def _data_small():
+    return gu.read_input(_DAY_NUM, "small")
+
+
+def _data_bigger():
+    return gu.read_input(_DAY_NUM, "bigger")
+
+
+def _data_p():
+    return gu.read_input(_DAY_NUM, "p")
+
+
+def test_parse_input():
+    """tests parse input with example data"""
+    actual = sol.parse_input(_data_small())
+    expected = [
+        sol.Move("R", 4),
+        sol.Move("U", 4),
+        sol.Move("L", 3),
+        sol.Move("D", 1),
+        sol.Move("R", 4),
+        sol.Move("D", 1),
+        sol.Move("L", 5),
+        sol.Move("R", 2),
+    ]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "input_head,input_tail,expected",
+    [
+        ((2, 2), (2, 2), (2, 2)),
+        ((2, 2), (1, 1), (1, 1)),
+        ((3, 1), (1, 1), (2, 1)),
+        ((1, 1), (1, 3), (1, 2)),
+        ((2, 3), (1, 1), (2, 2)),
+        ((3, 2), (1, 1), (2, 2)),
+    ],
+)
+def test_move_tail(input_head, input_tail, expected):
+    """tests move_tail"""
+    assert sol.move_tail(input_head, input_tail) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,rope_length,expected",
+    [
+        pytest.param(
+            _data_small(),
+            2,
+            {
+                (0, 0),
+                (1, 0),
+                (2, 0),
+                (3, 0),
+                (4, 1),
+                (4, 2),
+                (3, 2),
+                (2, 2),
+                (1, 2),
+                (3, 3),
+                (4, 3),
+                (2, 4),
+                (3, 4),
+            },
+            id="small",
+        ),
+        pytest.param(
+            _data_bigger(),
+            10,
+            {
+                (0, 0),
+                (1, 1),
+                (2, 2),
+                (1, 3),
+                (2, 4),
+                (3, 5),
+                (4, 5),
+                (5, 5),
+                (6, 4),
+                (7, 3),
+                (8, 2),
+                (9, 1),
+                (10, 0),
+                (9, -1),
+                (8, -2),
+                (7, -3),
+                (6, -4),
+                (5, -5),
+                (4, -5),
+                (3, -5),
+                (2, -5),
+                (1, -5),
+                (0, -5),
+                (-1, -5),
+                (-2, -5),
+                (-3, -4),
+                (-4, -3),
+                (-5, -2),
+                (-6, -1),
+                (-7, 0),
+                (-8, 1),
+                (-9, 2),
+                (-10, 3),
+                (-11, 4),
+                (-11, 5),
+                (-11, 6),
+            },
+            id="bigger",
+        ),
+    ],
+)
+def test_rope_simulator(input_str, rope_length, expected):
+    """tests RopeSimulator"""
+    rope_simulator = sol.RopeSimulator(rope_length)
+    rope_simulator.make_moves(sol.parse_input(input_str))
+    assert rope_simulator.visited_by_tail == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 13, id="small"),
+        pytest.param(_data_p(), 6090, id="p"),
+    ],
+)
+def test_solve_a(input_str, expected):
+    """tests solve_a"""
+    assert sol.solve_a(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 1, id="small"),
+        pytest.param(_data_bigger(), 36, id="bigger"),
+        pytest.param(_data_p(), 2566, id="p"),
+    ],
+)
+def test_solve_b(input_str, expected):
+    """tests solve_b"""
+    assert sol.solve_b(input_str) == expected
