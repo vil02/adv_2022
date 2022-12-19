94e8e2a97d34eaeb20183d759e18bb3413147629
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-12-19 22:39:05 +0100

    feat: add adv_2022_17 (#41)
    * feat: add adv_2022_17
    
    * tests: add `test_drop_single_returns_none_when_generator_exhausted`
    
    * style: simplify code
    
    * style: add _ to move_generator

---
+++ tests/test_adv_2022_17.py
@@ -0,0 +1,112 @@
+"""tests of adv_2022_17"""
+
+import pytest
+import general_utils as gu
+import solutions.adv_2022_17 as sol
+
+_DAY_NUM = 17
+
+
+def _data_blocks():
+    return gu.read_input(_DAY_NUM, "blocks")
+
+
+def _data_small():
+    return gu.read_input(_DAY_NUM, "small")
+
+
+def _data_p():
+    return gu.read_input(_DAY_NUM, "p")
+
+
+def test_parse_blocks():
+    """tests parse_blocks"""
+    expected = (
+        frozenset({(0, 0), (1, 0), (2, 0), (3, 0)}),
+        frozenset({(1, 0), (0, 1), (1, 1), (1, 2), (2, 1)}),
+        frozenset({(2, 1), (0, 0), (2, 0), (2, 2), (1, 0)}),
+        frozenset({(0, 0), (0, 1), (0, 2), (0, 3)}),
+        frozenset({(0, 0), (1, 0), (0, 1), (1, 1)}),
+    )
+    assert sol.parse_blocks(_data_blocks()) == expected
+
+
+def test_parse_input():
+    """tests parse input with example data"""
+    expected = (
+        ">",
+        ">",
+        ">",
+        "<",
+        "<",
+        ">",
+        "<",
+        ">",
+        ">",
+        "<",
+        "<",
+        "<",
+        ">",
+        ">",
+        "<",
+        ">",
+        ">",
+        ">",
+        "<",
+        "<",
+        "<",
+        ">",
+        ">",
+        ">",
+        "<",
+        "<",
+        "<",
+        ">",
+        "<",
+        "<",
+        "<",
+        ">",
+        ">",
+        "<",
+        ">",
+        ">",
+        "<",
+        "<",
+        ">",
+        ">",
+    )
+    assert sol.parse_input(_data_small()) == expected
+
+
+def test_drop_single_returns_none_when_generator_exhausted():
+    """checks that drop_single returns None when gen_moves exhausted"""
+
+    def _move_generator():
+        yield "<"
+        yield "<"
+
+    assert sol.drop_single(set(), {(0, 0), (0, 1)}, _move_generator()) is None
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 3068, id="small"),
+        pytest.param(_data_p(), 3181, id="p"),
+    ],
+)
+def test_solve_a(input_str, expected):
+    """tests solve_a"""
+    assert sol.solve_a(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 1514285714288, id="small"),
+        pytest.param(_data_p(), 1570434782634, id="p"),
+    ],
+)
+def test_solve_b(input_str, expected):
+    """tests solve_b"""
+    assert sol.solve_b(input_str) == expected
