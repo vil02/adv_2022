0e49fa61da3fd46654ded39c53a84c0956ecc39d
“vil02” <vil02@o2.pl>
2022-12-23 19:01:44 +0100

    refactor: simplify logic

--- solutions/adv_2022_23.py
+++ solutions/adv_2022_23.py
@@ -64,12 +64,6 @@ def _get_similar_dirs(in_dir_name):
     }[in_dir_name]
 
 
-def _are_all_empty(in_pos, in_dir_names, in_elves_positions):
-    return all(
-        _make_shift(in_pos, _get_dir(_)) not in in_elves_positions for _ in in_dir_names
-    )
-
-
 class Elf:
     """represents a moving elf"""
 
@@ -80,15 +74,21 @@ class Elf:
     def propose_pos(self, in_elves_positions, in_dir_names):
         """makes a position proposal"""
         self.new_pos = None
-        neighbours = _get_all_neighbours(self.pos)
-        if all(_ not in in_elves_positions for _ in neighbours):
+        neighbours = {
+            dir_name: _make_shift(self.pos, dir_shift)
+            for dir_name, dir_shift in _get_dirs_dict().items()
+        }
+        open_dirs = {
+            dir_name
+            for dir_name, pos in neighbours.items()
+            if pos not in in_elves_positions
+        }
+        if len(open_dirs) == 8:
             return self.new_pos
 
         for cur_dir_name in in_dir_names:
-            if _are_all_empty(
-                self.pos, _get_similar_dirs(cur_dir_name), in_elves_positions
-            ):
-                self.new_pos = _make_shift(self.pos, _get_dir(cur_dir_name))
+            if all(_ in open_dirs for _ in _get_similar_dirs(cur_dir_name)):
+                self.new_pos = neighbours[cur_dir_name]
                 break
         return self.new_pos
 
