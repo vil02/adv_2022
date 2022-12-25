636a9674450036c517d3907fd884b60b3c3b98b6
“vil02” <vil02@o2.pl>
2022-12-25 18:09:02 +0100

    feat: add adv_2022_25

---
+++ solutions/adv_2022_25.py
@@ -0,0 +1,54 @@
+"""solution of adv_2022_25"""
+
+
+def _snafu_to_decimal_single_digit(in_snafu_digit):
+    return {"0": 0, "1": 1, "2": 2, "-": -1, "=": -2}[in_snafu_digit]
+
+
+def snafu_to_decimal(in_snafu):
+    """converts a snafu number to decimal int"""
+    res = 0
+    multip = 5
+    for _ in in_snafu:
+        res *= multip
+        res += _snafu_to_decimal_single_digit(_)
+    return res
+
+
+def decimal_to_base_5(in_decimal):
+    res = []
+    cur_val = in_decimal
+    base = 5
+    while cur_val:
+        res.append(str(cur_val % base))
+        cur_val //= base
+    res = "".join(reversed(res))
+    assert int(res, base=base) == in_decimal
+    return res
+
+
+def decimal_to_snafu(in_decimal):
+    """converts a decmal int into snafu"""
+    reversed_base_5 = list(reversed(decimal_to_base_5(in_decimal)))
+    carry_over = 0
+    res = []
+    for _ in reversed_base_5:
+        cur_digit = int(_)
+        cur_digit += carry_over
+        carry_over = 0
+        if cur_digit in {3, 4}:
+            cur_digit = "=" if cur_digit == 3 else "-"
+            carry_over = 2 if cur_digit == 3 else 1
+        elif cur_digit == 5:
+            cur_digit = "0"
+            carry_over = 1
+        res.append(str(cur_digit))
+    if carry_over:
+        res.append(str(carry_over))
+    return "".join(reversed(res))
+
+
+def solve_a(in_str):
+    """returns the sum of the inpus snafu numbers as snafu number"""
+    res_dec = sum(snafu_to_decimal(_) for _ in in_str.splitlines())
+    return decimal_to_snafu(res_dec)
