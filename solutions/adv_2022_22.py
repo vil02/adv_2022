ad3d5dbf5082e0a8493609abc9ee407eb2364625
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2022-12-26 23:29:34 +0100

    feat: add adv_2022 (#51)
    * tests: add test data for adv_2022_22
    
    * tests: fix test data
    
    * fix: remove blank line at the end of the files
    
    * feat: add adv_2022_22 part_a
    
    * feat: add adv_2022_22_b
    
    * refactor: simplify _compute_wrap_data
    
    * feat: check if side_length >= 2 in EdgeWalker
    
    * style: linting
    
    * style: add line breaks
    
    * tests: add test_mover_wrap_pos_is_abstract
    
    * docs: add doc string to Mover

---
+++ solutions/adv_2022_22.py
@@ -0,0 +1,391 @@
+"""solution of adv_2022_22"""
+
+import collections
+import string
+import math
+
+
+def _to_pos(in_x, in_y):
+    return (in_x, in_y)
+
+
+def _parse_map(in_str):
+    map_data = {}
+    start_pos = None
+    for pos_y, row in enumerate(in_str.splitlines(), start=1):
+        for pos_x, cur_char in enumerate(row, start=1):
+            if cur_char != " ":
+                if start_pos is None:
+                    assert cur_char != "#"
+                    start_pos = _to_pos(pos_x, pos_y)
+                assert cur_char in {".", "#"}
+                map_data[_to_pos(pos_x, pos_y)] = cur_char
+    assert start_pos is not None
+    return map_data, start_pos
+
+
+def _parse_moves(in_str):
+    def _to_int(in_num_chars):
+        return int("".join(in_num_chars))
+
+    res = []
+    cur_num_chars = []
+    for _ in in_str:
+        if _ in {"L", "R"}:
+            res.append(_to_int(cur_num_chars))
+            cur_num_chars = []
+            res.append(_)
+        else:
+            assert _ in string.digits
+            cur_num_chars.append(_)
+    assert cur_num_chars
+    res.append(_to_int(cur_num_chars))
+    return res
+
+
+def parse_input(in_str):
+    """parses the input"""
+    map_str, moves_str = in_str.split("\n\n")
+    map_data, start_pos = _parse_map(map_str)
+    return collections.namedtuple("ParsingResult", ["map", "start_pos", "moves"])(
+        map_data, start_pos, _parse_moves(moves_str.strip())
+    )
+
+
+def _change_dir(in_dir, in_turn):
+    new_dirs = {
+        (1, 0): (0, -1),
+        (0, -1): (-1, 0),
+        (-1, 0): (0, 1),
+        (0, 1): (1, 0),
+    }
+    if in_turn == "R":
+        new_dirs = {val: key for key, val in new_dirs.items()}
+    else:
+        assert in_turn == "L"
+    return new_dirs[in_dir]
+
+
+def _add_tuples(*in_tuples):
+    return tuple(sum(_) for _ in zip(*in_tuples))
+
+
+def _make_shift(in_pos, in_shift):
+    return _add_tuples(in_pos, in_shift)
+
+
+def _multiply_tuple(in_scalar, in_tuple):
+    return tuple(in_scalar * _ for _ in in_tuple)
+
+
+def _negate_tuple(in_tuple):
+    return _multiply_tuple(-1, in_tuple)
+
+
+class Mover:
+    """abstract class providing functionalities related to moving on maps"""
+
+    def __init__(self, map_data, start_pos):
+        assert start_pos in map_data
+        self._map = map_data
+        self._cur_pos = start_pos
+        self._cur_dir = (1, 0)
+
+    def wrap_pos(self, in_pos, in_dir):
+        """returns the position/direction after the move outside"""
+        raise NotImplementedError()
+
+    def _change_dir(self, in_turn):
+        self._cur_dir = _change_dir(self.cur_dir, in_turn)
+
+    def _is_empty(self, in_pos):
+        return self._map[in_pos] == "."
+
+    def _single_step(self):
+        new_pos = _make_shift(self.cur_pos, self.cur_dir)
+        if new_pos in self._map:
+            if self._is_empty(new_pos):
+                self._cur_pos = new_pos
+            return
+        wraped_pos, wraped_dir = self.wrap_pos(self.cur_pos, self.cur_dir)
+        if self._is_empty(wraped_pos):
+            self._cur_pos = wraped_pos
+            self._cur_dir = wraped_dir
+
+    def make_move(self, in_move):
+        """makes the single move"""
+        if in_move in {"L", "R"}:
+            self._change_dir(in_move)
+        else:
+            for _ in range(in_move):
+                self._single_step()
+
+    def make_moves(self, in_moves):
+        """makes all of the moves in_moves"""
+        for _ in in_moves:
+            self.make_move(_)
+
+    @property
+    def cur_pos(self):
+        """getter for _cur_pos"""
+        return self._cur_pos
+
+    @property
+    def cur_dir(self):
+        """getter for _cur_dir"""
+        return self._cur_dir
+
+
+class MoverFlat(Mover):
+    """imlements a Mover class for a walk on a flat map"""
+
+    def __init__(self, map_data, start_pos):
+        super().__init__(map_data, start_pos)
+        self._wraps = {}
+
+    def wrap_pos(self, in_pos, in_dir):
+        """wraps the position as in part a"""
+        assert in_pos in self._map
+        if (in_pos, in_dir) in self._wraps:
+            return self._wraps[(in_pos, in_dir)]
+        search_dir = _negate_tuple(in_dir)
+        tmp_pos = in_pos
+        while tmp_pos in self._map:
+            tmp_pos = _make_shift(tmp_pos, search_dir)
+        res = _make_shift(tmp_pos, in_dir), in_dir
+        self._wraps[(in_pos, in_dir)] = res
+        return res
+
+
+def _facing(in_dir):
+    return {(1, 0): 0, (0, 1): 1, (-1, 0): 2, (0, -1): 3}[in_dir]
+
+
+def get_pwd(in_pos, in_dir):
+    """returns the password of given positon/direction"""
+    return 1000 * in_pos[1] + 4 * in_pos[0] + _facing(in_dir)
+
+
+def _solve(in_str, mover_type):
+    parse_res = parse_input(in_str)
+    mover = mover_type(parse_res.map, parse_res.start_pos)
+    mover.make_moves(parse_res.moves)
+    return get_pwd(mover.cur_pos, mover.cur_dir)
+
+
+def solve_a(in_str):
+    """returns the solution for part_a"""
+    return _solve(in_str, MoverFlat)
+
+
+def _get_all_dirs():
+    return (
+        (1, 0),
+        (1, -1),
+        (0, -1),
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    )
+
+
+def _get_main_dirs():
+    return (
+        (1, 0),
+        (0, -1),
+        (-1, 0),
+        (0, 1),
+    )
+
+
+def _calculate_maxs(in_map):
+    x_max = 0
+    y_max = 0
+    for x_pos, y_pos in in_map:
+        x_max = max(x_max, x_pos)
+        y_max = max(y_max, y_pos)
+    return x_max, y_max
+
+
+def compute_side_length(in_net):
+    """returns the length of the cube with net represented by in_net"""
+    side_length = math.gcd(*_calculate_maxs(in_net))
+    assert len(in_net) == 6 * side_length**2
+    return side_length
+
+
+WalkData = collections.namedtuple("WalkData", ["pos", "dir"])
+
+
+class EdgeWalker:
+    """
+    implements functionalities related to moving along the edges of a net of a cube
+    """
+
+    def __init__(self, in_net):
+        assert compute_side_length(in_net) >= 2
+        self.net = in_net
+
+    def is_in(self, in_pos):
+        """returns True iff in_pos is inside the net"""
+        return in_pos in self.net
+
+    def _is_shift_in(self, in_pos, in_shift):
+        return self.is_in(_make_shift(in_pos, in_shift))
+
+    def _is_shift_on_edge(self, in_pos, in_shift):
+        return self.is_on_edge(_make_shift(in_pos, in_shift))
+
+    def is_on_edge(self, in_pos):
+        """checks if given position is on the edge of the net"""
+        assert in_pos in self.net
+        return any(not self._is_shift_in(in_pos, _) for _ in _get_all_dirs())
+
+    def is_convex_corner(self, in_pos):
+        """checks if in_pos is on a outer/convex corner"""
+        assert self.is_on_edge(in_pos)
+        return sum(1 for _ in _get_all_dirs() if self._is_shift_in(in_pos, _)) == 3
+
+    def is_concave_corner(self, in_pos):
+        """checks if in_pos is on a inner/concave corner"""
+        assert self.is_on_edge(in_pos)
+        return sum(1 for _ in _get_all_dirs() if self._is_shift_in(in_pos, _)) == 7
+
+    def get_edge_dirs(self, in_pos):
+        """returns all directions along the edges at given position"""
+        assert self.is_on_edge(in_pos)
+        return [
+            _
+            for _ in _get_main_dirs()
+            if self._is_shift_in(in_pos, _) and self._is_shift_on_edge(in_pos, _)
+        ]
+
+    def get_dir_inside(self, in_pos):
+        """
+        Returns a direction "inside" the cubes net.
+        in_pos cannot be on the corner.
+        """
+        assert (
+            self.is_on_edge(in_pos)
+            and not self.is_convex_corner(in_pos)
+            and not self.is_concave_corner(in_pos)
+        )
+        dirs = [
+            _
+            for _ in _get_main_dirs()
+            if self._is_shift_in(in_pos, _) and not self._is_shift_on_edge(in_pos, _)
+        ]
+        assert len(dirs) == 1
+        return dirs[0]
+
+    def next_pos(self, in_walk_data):
+        """returns the data describing the next position in a walk along the edge"""
+        assert self.is_on_edge(in_walk_data.pos)
+        next_pos = _make_shift(in_walk_data.pos, in_walk_data.dir)
+        if self.is_in(next_pos) and self.is_on_edge(next_pos):
+            return WalkData(next_pos, in_walk_data.dir)
+        edge_dirs = self.get_edge_dirs(in_walk_data.pos)
+        assert len(edge_dirs) == 2
+        assert self.is_concave_corner(in_walk_data.pos) or self.is_convex_corner(
+            in_walk_data.pos
+        )
+        edge_dirs.remove(_negate_tuple(in_walk_data.dir))
+        return WalkData(in_walk_data.pos, edge_dirs[0])
+
+    def next_positions(self, *in_walks):
+        """applies next_pos to all entries of in_walks"""
+        return tuple(self.next_pos(_) for _ in in_walks)
+
+    def find_all_concave_corners(self):
+        """returns all of the concave corners"""
+        return {_ for _ in self.net if self.is_on_edge(_) and self.is_concave_corner(_)}
+
+
+def _update_wrap_data(wrap_data, in_pos_a, inside_dir_a, in_pos_b, inside_dir_b):
+    wrap_data[(in_pos_a, _negate_tuple(inside_dir_a))] = (in_pos_b, inside_dir_b)
+    wrap_data[(in_pos_b, _negate_tuple(inside_dir_b))] = (in_pos_a, inside_dir_a)
+
+
+def _update_wrap_data_at_regular_pos(wrap_data, edge_walker, in_pos_a, in_pos_b):
+    _update_wrap_data(
+        wrap_data,
+        in_pos_a,
+        edge_walker.get_dir_inside(in_pos_a),
+        in_pos_b,
+        edge_walker.get_dir_inside(in_pos_b),
+    )
+
+
+def _get_next_to_convex(edge_walker, walk_on_convex):
+    tmp_dir = (
+        walk_on_convex.dir
+        if edge_walker.is_in(_make_shift(walk_on_convex.pos, walk_on_convex.dir))
+        else _negate_tuple(walk_on_convex.dir)
+    )
+
+    return edge_walker.next_pos(WalkData(walk_on_convex.pos, tmp_dir))
+
+
+def _update_wrap_data_at_convex_pos(wrap_data, edge_walker, walk_on_convex, in_pos):
+    assert edge_walker.is_convex_corner(walk_on_convex.pos)
+    next_to_convex = _get_next_to_convex(edge_walker, walk_on_convex)
+    convex_insde_dir = edge_walker.get_dir_inside(next_to_convex.pos)
+    _update_wrap_data(
+        wrap_data,
+        walk_on_convex.pos,
+        convex_insde_dir,
+        in_pos,
+        edge_walker.get_dir_inside(in_pos),
+    )
+
+
+def _update_wrap_data_single_state(wrap_data, edge_walker, start_pos):
+    start_dirs = edge_walker.get_edge_dirs(start_pos)
+    assert len(start_dirs) == 2
+    walk_a, walk_b = edge_walker.next_positions(
+        *tuple(WalkData(start_pos, _) for _ in start_dirs)
+    )
+    while not (
+        edge_walker.is_convex_corner(walk_a.pos)
+        and edge_walker.is_convex_corner(walk_b.pos)
+    ):
+        if not edge_walker.is_convex_corner(
+            walk_a.pos
+        ) and not edge_walker.is_convex_corner(walk_b.pos):
+            _update_wrap_data_at_regular_pos(
+                wrap_data, edge_walker, walk_a.pos, walk_b.pos
+            )
+        elif edge_walker.is_convex_corner(walk_a.pos):
+            _update_wrap_data_at_convex_pos(wrap_data, edge_walker, walk_a, walk_b.pos)
+        else:
+            assert edge_walker.is_convex_corner(walk_b.pos)
+            _update_wrap_data_at_convex_pos(wrap_data, edge_walker, walk_b, walk_a.pos)
+        walk_a, walk_b = edge_walker.next_positions(walk_a, walk_b)
+
+
+def _compute_wrap_data(in_net):
+    edge_walker = EdgeWalker(in_net)
+    res = {}
+    for start_pos in edge_walker.find_all_concave_corners():
+        _update_wrap_data_single_state(res, edge_walker, start_pos)
+    return res
+
+
+class MoverCube(Mover):
+    """impelements a Mover class in case of a walk on a net of a cube"""
+
+    def __init__(self, map_data, start_pos):
+        super().__init__(map_data, start_pos)
+        self._wrap_data = _compute_wrap_data(self._map)
+
+    def wrap_pos(self, in_pos, in_dir):
+        """wraps the position as in part b"""
+        assert in_pos in self._map
+        return self._wrap_data[(in_pos, in_dir)]
+
+
+def solve_b(in_str):
+    """returns the solution for part_b"""
+    return _solve(in_str, MoverCube)
---
+++ tests/test_adv_2022_22.py
@@ -0,0 +1,395 @@
+"""tests of adv_2022_22"""
+
+import pytest
+import general_utils as gu
+import solutions.adv_2022_22 as sol
+
+_DAY_NUM = 22
+
+
+def _data_small():
+    return gu.read_input(_DAY_NUM, "small")
+
+
+def _data_p():
+    return gu.read_input(_DAY_NUM, "p")
+
+
+def test_parse_input():
+    """tests parse input with example data"""
+    parse_res = sol.parse_input(_data_small())
+    assert parse_res.moves == [10, "R", 5, "L", 5, "R", 10, "L", 4, "R", 5, "L", 5]
+    assert parse_res.start_pos == (9, 1)
+    assert parse_res.map == {
+        (9, 1): ".",
+        (10, 1): ".",
+        (11, 1): ".",
+        (12, 1): "#",
+        (9, 2): ".",
+        (10, 2): "#",
+        (11, 2): ".",
+        (12, 2): ".",
+        (9, 3): "#",
+        (10, 3): ".",
+        (11, 3): ".",
+        (12, 3): ".",
+        (9, 4): ".",
+        (10, 4): ".",
+        (11, 4): ".",
+        (12, 4): ".",
+        (1, 5): ".",
+        (2, 5): ".",
+        (3, 5): ".",
+        (4, 5): "#",
+        (5, 5): ".",
+        (6, 5): ".",
+        (7, 5): ".",
+        (8, 5): ".",
+        (9, 5): ".",
+        (10, 5): ".",
+        (11, 5): ".",
+        (12, 5): "#",
+        (1, 6): ".",
+        (2, 6): ".",
+        (3, 6): ".",
+        (4, 6): ".",
+        (5, 6): ".",
+        (6, 6): ".",
+        (7, 6): ".",
+        (8, 6): ".",
+        (9, 6): "#",
+        (10, 6): ".",
+        (11, 6): ".",
+        (12, 6): ".",
+        (1, 7): ".",
+        (2, 7): ".",
+        (3, 7): "#",
+        (4, 7): ".",
+        (5, 7): ".",
+        (6, 7): ".",
+        (7, 7): ".",
+        (8, 7): "#",
+        (9, 7): ".",
+        (10, 7): ".",
+        (11, 7): ".",
+        (12, 7): ".",
+        (1, 8): ".",
+        (2, 8): ".",
+        (3, 8): ".",
+        (4, 8): ".",
+        (5, 8): ".",
+        (6, 8): ".",
+        (7, 8): ".",
+        (8, 8): ".",
+        (9, 8): ".",
+        (10, 8): ".",
+        (11, 8): "#",
+        (12, 8): ".",
+        (9, 9): ".",
+        (10, 9): ".",
+        (11, 9): ".",
+        (12, 9): "#",
+        (13, 9): ".",
+        (14, 9): ".",
+        (15, 9): ".",
+        (16, 9): ".",
+        (9, 10): ".",
+        (10, 10): ".",
+        (11, 10): ".",
+        (12, 10): ".",
+        (13, 10): ".",
+        (14, 10): "#",
+        (15, 10): ".",
+        (16, 10): ".",
+        (9, 11): ".",
+        (10, 11): "#",
+        (11, 11): ".",
+        (12, 11): ".",
+        (13, 11): ".",
+        (14, 11): ".",
+        (15, 11): ".",
+        (16, 11): ".",
+        (9, 12): ".",
+        (10, 12): ".",
+        (11, 12): ".",
+        (12, 12): ".",
+        (13, 12): ".",
+        (14, 12): ".",
+        (15, 12): "#",
+        (16, 12): ".",
+    }
+
+
+def test_mover_wrap_pos_is_abstract():
+    """tests if wrap_pos in Mover is not implemented"""
+    mover = sol.Mover({(0, 1): "."}, (0, 1))
+    with pytest.raises(NotImplementedError):
+        mover.wrap_pos((0, 1), (0, 1))
+
+
+@pytest.mark.parametrize(
+    "input_pos, input_dir, expected_pos, expected_dir",
+    [
+        ((3, 5), (0, -1), (3, 8), (0, -1)),
+        ((1, 5), (-1, 0), (12, 5), (-1, 0)),
+        ((2, 8), (0, 1), (2, 5), (0, 1)),
+        ((12, 5), (1, 0), (1, 5), (1, 0)),
+    ],
+)
+def test_mover_flat_wrap_pos(input_pos, input_dir, expected_pos, expected_dir):
+    """tests wrap_pos in MoverFlat"""
+    parse_res = sol.parse_input(_data_small())
+    mover = sol.MoverFlat(parse_res.map, parse_res.start_pos)
+    assert mover.wrap_pos(input_pos, input_dir) == (expected_pos, expected_dir)
+
+
+@pytest.mark.parametrize(
+    "input_pos,input_dir,expected",
+    [
+        ((8, 6), (1, 0), 6032),
+        ((7, 5), (0, -1), 5031),
+        ((2, 4), (-1, 0), 1000 * 4 + 4 * 2 + 2),
+    ],
+)
+def test_get_pwd(input_pos, input_dir, expected):
+    """tests get_pwd"""
+    assert sol.get_pwd(input_pos, input_dir) == expected
+
+
+def test_make_move_mover_a():
+    """tests make_move in part a"""
+    parse_res = sol.parse_input(_data_small())
+    mover = sol.MoverFlat(parse_res.map, parse_res.start_pos)
+    expected = [
+        ((11, 1), (1, 0)),
+        ((11, 1), (0, 1)),
+        ((11, 6), (0, 1)),
+        ((11, 6), (1, 0)),
+        ((4, 6), (1, 0)),
+        ((4, 6), (0, 1)),
+        ((4, 8), (0, 1)),
+        ((4, 8), (1, 0)),
+        ((8, 8), (1, 0)),
+        ((8, 8), (0, 1)),
+        ((8, 6), (0, 1)),
+        ((8, 6), (1, 0)),
+    ]
+
+    for cur_move, ex_data in zip(parse_res.moves, expected):
+        mover.make_move(cur_move)
+        assert (mover.cur_pos, mover.cur_dir) == ex_data
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 6032, id="small"),
+        pytest.param(_data_p(), 181128, id="p"),
+    ],
+)
+def test_solve_a(input_str, expected):
+    """tests solve_a"""
+    assert sol.solve_a(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 4, id="small"),
+        pytest.param(_data_p(), 50, id="p"),
+    ],
+)
+def test_compute_side_length(input_str, expected):
+    """tests compute_side_length"""
+    assert sol.compute_side_length(sol.parse_input(input_str).map) == expected
+
+
+_SMALL_EDGE_WALKER = sol.EdgeWalker(sol.parse_input(_data_small()).map)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (9, 1)),
+        (_SMALL_EDGE_WALKER, (9, 4)),
+        (_SMALL_EDGE_WALKER, (9, 5)),
+        (_SMALL_EDGE_WALKER, (8, 5)),
+        (_SMALL_EDGE_WALKER, (1, 5)),
+        (_SMALL_EDGE_WALKER, (1, 6)),
+        (_SMALL_EDGE_WALKER, (16, 12)),
+        (_SMALL_EDGE_WALKER, (16, 11)),
+        (_SMALL_EDGE_WALKER, (15, 12)),
+    ],
+)
+def test_is_on_edge_positive(edge_walker, pos):
+    """positive test of is_on_edge"""
+    assert edge_walker.is_on_edge(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (10, 2)),
+        (_SMALL_EDGE_WALKER, (10, 2)),
+        (_SMALL_EDGE_WALKER, (9, 6)),
+        (_SMALL_EDGE_WALKER, (10, 5)),
+    ],
+)
+def test_is_on_edge_negative(edge_walker, pos):
+    """negative test of is_on_edge"""
+    assert not edge_walker.is_on_edge(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (9, 1)),
+        (_SMALL_EDGE_WALKER, (1, 5)),
+        (_SMALL_EDGE_WALKER, (1, 8)),
+        (_SMALL_EDGE_WALKER, (16, 12)),
+    ],
+)
+def test_is_convex_corner_positive(edge_walker, pos):
+    """positive test of is_convex_corner"""
+    assert edge_walker.is_convex_corner(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (9, 4)),
+        (_SMALL_EDGE_WALKER, (9, 5)),
+        (_SMALL_EDGE_WALKER, (8, 5)),
+    ],
+)
+def test_is_convex_corner_negative(edge_walker, pos):
+    """negative test of is_convex_corner"""
+    assert not edge_walker.is_convex_corner(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (9, 5)),
+        (_SMALL_EDGE_WALKER, (9, 8)),
+        (_SMALL_EDGE_WALKER, (12, 9)),
+    ],
+)
+def test_is_concave_corner_positive(edge_walker, pos):
+    """positive test of is_concave_corner"""
+    assert edge_walker.is_concave_corner(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, pos",
+    [
+        (_SMALL_EDGE_WALKER, (9, 4)),
+        (_SMALL_EDGE_WALKER, (8, 5)),
+        (_SMALL_EDGE_WALKER, (4, 5)),
+        (_SMALL_EDGE_WALKER, (9, 1)),
+        (_SMALL_EDGE_WALKER, (13, 9)),
+    ],
+)
+def test_is_concave_corner_negative(edge_walker, pos):
+    """negative test of is_concave_corner"""
+    assert not edge_walker.is_concave_corner(pos)
+
+
+@pytest.mark.parametrize(
+    "edge_walker, input_walk_data, expected_walk_data",
+    [
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((4, 5), (1, 0)),
+            sol.WalkData((5, 5), (1, 0)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 1), (0, -1)),
+            sol.WalkData((9, 1), (1, 0)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 1), (1, 0)),
+            sol.WalkData((10, 1), (1, 0)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 1), (-1, 0)),
+            sol.WalkData((9, 1), (0, 1)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 1), (0, 1)),
+            sol.WalkData((9, 2), (0, 1)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((8, 8), (1, 0)),
+            sol.WalkData((9, 8), (1, 0)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 8), (1, 0)),
+            sol.WalkData((9, 8), (0, 1)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 8), (0, 1)),
+            sol.WalkData((9, 9), (0, 1)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 9), (0, -1)),
+            sol.WalkData((9, 8), (0, -1)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 8), (0, -1)),
+            sol.WalkData((9, 8), (-1, 0)),
+        ),
+        (
+            _SMALL_EDGE_WALKER,
+            sol.WalkData((9, 8), (-1, 0)),
+            sol.WalkData((8, 8), (-1, 0)),
+        ),
+    ],
+)
+def test_next_pos(edge_walker, input_walk_data, expected_walk_data):
+    """tests next_pos"""
+    assert edge_walker.next_pos(input_walk_data) == expected_walk_data
+
+
+@pytest.mark.parametrize(
+    "edge_walker, expected",
+    [
+        (_SMALL_EDGE_WALKER, {(9, 5), (9, 8), (12, 9)}),
+    ],
+)
+def test_find_all_concave_corners(edge_walker, expected):
+    """tests find_all_concave_corners"""
+    assert edge_walker.find_all_concave_corners() == expected
+
+
+@pytest.mark.parametrize(
+    "edge_walker, input_pos, expected",
+    [
+        (_SMALL_EDGE_WALKER, (4, 5), (0, 1)),
+        (_SMALL_EDGE_WALKER, (9, 2), (1, 0)),
+    ],
+)
+def test_get_dir_inside(edge_walker, input_pos, expected):
+    """tests get_dir_inside"""
+    assert edge_walker.get_dir_inside(input_pos) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected",
+    [
+        pytest.param(_data_small(), 5031, id="small"),
+        pytest.param(_data_p(), 52311, id="p"),
+    ],
+)
+def test_solve_b(input_str, expected):
+    """tests solve_b"""
+    assert sol.solve_b(input_str) == expected
