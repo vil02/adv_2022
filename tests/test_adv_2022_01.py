5ebcaac32a5ed3b0d113a7d3bce8d4193d650f09
piotr.idzik <vil02@o2.pl>
2023-08-14 21:38:19 +0200

    tests: add test data for `adv_2022_01`

--- tests/test_adv_2022_01.py
+++ tests/test_adv_2022_01.py
@@ -19,6 +19,10 @@ def _data_f():
     return gu.read_input(_DAY_NUM, "f")
 
 
+def _data_b():
+    return gu.read_input(_DAY_NUM, "b")
+
+
 def test_parse_input():
     """tests parse_input agains example data"""
     expected = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
@@ -32,6 +36,7 @@ def test_parse_input():
         (_data_small(), 24000),
         (_data_p(), 69912),
         (_data_f(), 66487),
+        (_data_b(), 68923),
     ],
 )
 def test_solve_a(input_str, expected):
@@ -45,6 +50,7 @@ def test_solve_a(input_str, expected):
         (_data_small(), 45000),
         (_data_p(), 208180),
         (_data_f(), 197301),
+        (_data_b(), 200044),
     ],
 )
 def test_solve_b(input_str, expected):
